ecb38b12f14ea95c3a6cfb0c2a8dbbc7a3074475
Bruno Salata <brunoslima@gmail.com>
2022-08-05 20:09:45 -0300

    Correções dependência

--- src/main/java/one/digitalinnovation/ProjetoJavaPadroes/service/ViaCepService.java
+++ src/main/java/one/digitalinnovation/ProjetoJavaPadroes/service/ViaCepService.java
@@ -1,8 +1,8 @@
 package one.digitalinnovation.ProjetoJavaPadroes.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import one.digitalinnovation.ProjetoJavaPadroes.model.Endereco;
 
@@ -13,11 +13,10 @@ import one.digitalinnovation.ProjetoJavaPadroes.model.Endereco;
  * @see <a href="httpsd://viacep.com.br">ViaCEP</a>
  */
 
-@FeignClient(namo = "viacep", url = "https://viacep.com.br/ws")
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 	
-	// @GetMapping("/{cep}/json/)
-	@RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
-	Endereco consultarCep(@PathVariable("cep" String cep);
+	@GetMapping("/{cep}/json/")
+	Endereco consultarCep(@PathVariable("cep") String cep);
 
 }
